3896c68d5fadf778cbb2375bbf19b5782d94a10d
Likith Reddy S <128393313+likithReddyBayer@users.noreply.github.com>
2024-09-27 10:50:11 +0530

    base

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react"
+import { Todo } from "../components/model";
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
+import { TiTickOutline } from "react-icons/ti";
+import "./styles.css";
+import TodoList from "./TodoList";
+
+
+type Props = {
+    todo:Todo;
+    todos:Todo[];
+    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
+};
+
+const SingleTodo =({todo,todos,setTodos}:Props) =>{
+    const[edit,setEdit]=useState<boolean>(false);
+    const[editTodo,setEditTodo]=useState<string>(todo.todo);
+    const handleDone=(id:number)=>{
+        setTodos(
+        todos.map((todo)=>todo.id===id?{...todo,isDone: !todo.isDone }:todo)
+        );
+    };
+
+    const handleDelete=(id:number)=>{
+        setTodos(todos.filter((todo)=> todo.id!==id));
+    };
+    const handleEdit=(e:React.FormEvent,id:number)=>{
+    e.preventDefault();
+
+    setTodos(todos.map((todo)=>(
+        todo.id===id?{...todo,todo:editTodo}:todo
+    )));
+    setEdit(false);
+    };
+
+        return <form className="todos_single" onSubmit={(e)=>handleEdit(e,todo.id)}>
+
+            {
+              edit?(
+              <input value={editTodo}onChange={(e)=>setEditTodo(e.target.value)} className="todos_single--text"/>
+              ):todo.isDone?(
+                    <s className="todos_single--text">{todo.todo}</s>
+                ):(
+                    <span className="todos_single--text">{todo.todo}</span>
+                )}
+            <div>
+                <span className="icon" onClick={()=>{
+                if(!edit && !todo.isDone){
+                    setEdit(!edit);
+                }
+                
+                }}>
+                <CiEdit />
+                </span>
+                <span className="icon" onClick={()=>handleDelete(todo.id)}><MdDelete />
+                </span>
+                <span className="icon" onClick={()=>handleDone(todo.id)}><TiTickOutline /></span>
+            </div>
+        
+
+    </form>
+};
+
+export default SingleTodo;
